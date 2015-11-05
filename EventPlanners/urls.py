2cd545ffdcbabb2f16ef91a1d06f2a44087bf1df
Supriya <sup0508@gmail.com>
2015-11-05 19:32:12 +0530

    Caterers page linked. Images added to front page.

--- EventPlanners/urls.py
+++ EventPlanners/urls.py
@@ -2,6 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.caterers_list, name='caterers_list'),
+    #url(r'^$', views.caterers_list, name='caterers_list'),
+    url(r'^$', views.frontpage, name='frontpage'),
+    url(r'^caterers_list.html$', views.caterers_list, name='caterers_list'),
 ]
 
--- EventPlanners/views.py
+++ EventPlanners/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from .models import Caterers
 
+
+def frontpage(request):
+    return render(request, 'EventPlanners/frontpage.html', {})
+
+
 def caterers_list(request):
     cats = Caterers.objects.all()
     return render(request, 'EventPlanners/caterers_list.html', {'cats': cats})
